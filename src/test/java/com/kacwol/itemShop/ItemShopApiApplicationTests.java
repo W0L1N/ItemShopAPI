8ed51e6c00b07b4fb9fd6e8c3ec0e41894e54ebe
Kacper Wolinski <kacperwolinski404@gmail.com>
2024-12-21 20:42:05 +0100

    Initial commit

---
+++ src/test/java/com/kacwol/itemShop/ItemShopApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kacwol.itemShop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItemShopApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
